8224fc44ab01841fe012618fe26b0fdffa2654ed
EV <67662631+ElizabethVorotnicoff2020@users.noreply.github.com>
2022-07-21 13:56:18 -0300

    add file index lading

---
+++ Funciones.py
@@ -0,0 +1,15 @@
+# print("Hola, World, Everyone")
+
+# Lista_1 = [1, 4, 10, 25]
+Lista_2 = ["john", "Lenon", "Smith", "Juan"]
+# Lista_3 = [True, False, False, True]
+# Lista_4 = [25.1, 15.8, 8.6]
+# Lista_vacia = []
+# Lista_compuesta = ["world", 15, True, 14.8]
+# frase = "Hoy es Domingo"
+
+# for i in frase:
+#     print(i)
+
+# print( max (Lista_1))
+print("El maximo de la lista 2 es", max (Lista_2))
